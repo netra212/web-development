97a1516641abee1bfa82b09512f49f9dccdc1dc5
netra212 <netra200021kcbdr@gmail.com>
2025-07-24 10:18:30 +0100

    Promises

--- web2/week_2/promises.js
+++ web2/week_2/promises.js
@@ -14,15 +14,80 @@ class Rectangle{
     paint(){
         console.log(`Painting with color : ${this.color}`);
     }
-
-    destroy(){
-
-    }
 }
 
 const rect = new Rectangle(2, 4, "red");
 const area = rect.area();
 console.log(rect.paint());
 console.log(area);
+
+// Date. 
+const now = new Date(); // Current date and time. 
+console.log(now.toISOString());
+console.log(now.getDay());
+console.log(now.getMonth());
+console.log(now.getFullYear());
+
+// Map. 
+const map = new Map();
+map.set(
+    'name', 'Alice'
+);
+map.set(
+    'age',
+    30
+);
+
+const firstName = map.get("name");
+console.log(map.get('name'));
+
+// 1. Introduced how to create a class and use it - hard (new)
+// 2. We used some JS provided classes (Map, Date)
+// 3. We are going to introduce the `Promise` class. 
+
+// Promise class gives us a promise, that I will return u somethig in the future. 
+// setTimeout(fn, 3000)
+function logName(){
+    console.log("Netra khatri...!");
+}
+
+setTimeout(logName, 3000);
+
+// callback based approach.  
+// promise based approach. 
+
+// Defining a promise is hard. 
+// Using a promise is easy. 
+
+
+// Promises. 
+// A Promise in Javascript is an object that represents the eventual completion (or failure) of an asynchronous operation and is resulting value. 
+
+function main(){
+
+}
+
+setTimeout(main, 3000); // once the 3 seconds passed, then callback the `main` function. 
+// Callback version. 
+// setTimeout(callback, 3000)
+
+// Promisified version.
+// setTimeoutPromisified(3000).then(callback);
+
+function setTimeoutPromisified(ms){
+    let p = new Promise(resolve => setTimeout(resolve, ms));
+    console.log("something happening here...");
+    console.log("Promises are just a syntactically superior way to write instead of callback.")
+    return p;
+
+    // returning the object of the Promise classes. 
+}
+
+function callback(){
+    console.log("3 seconds have been passed.");
+}
+
+setTimeoutPromisified(3000).then(callback);
+
 
 
