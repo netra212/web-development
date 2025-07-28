225f139219a593099c2e11371074fd6e6ec503c2
netra212 <netra200021kcbdr@gmail.com>
2025-07-28 21:01:05 +0100

    adding index.js

--- web2/week_4/index.js
+++ web2/week_4/index.js
@@ -22,23 +22,55 @@
 // })
 
 // Create a Command Line Interface.: that lets the user specify a file path and 
-const fs = require("fs");
-const { Command } = require("commander");
+// const fs = require("fs");
+// const { Command } = require("commander");
 
-const program = new Command();
+// const program = new Command();
 
-program.name("counter").description("CLI to do file based tasks.").version("0.8.0");
+// program.name("counter").description("CLI to do file based tasks.").version("0.8.0");
 
-program.command("count").description("Count the number of lines in a file.").argument("<file>", "file to count").action((file) => {
-    fs.readFile("a.txt", "utf-8", (err, data) => {
-        if (err){
-            console.log(err);
-        }
-        else{
-            const lines = data.split("\n").length;
-            console.log(`There are ${lines} lines in ${file}`);
-        }
-    })
+// program.command("count").description("Count the number of lines in a file.").argument("<file>", "file to count").action((file) => {
+//     fs.readFile("a.txt", "utf-8", (err, data) => {
+//         if (err){
+//             console.log(err);
+//         }
+//         else{
+//             const lines = data.split("\n").length;
+//             console.log(`There are ${lines} lines in ${file}`);
+//         }
+//     })
+// })
+
+// program.parse();
+
+const experss = require("express");
+
+const app = experss();
+
+// Define route handlers. 
+app.get("/", function(req, res){
+    res.send("Hello World.");
 })
 
-program.parse();
+app.get("/asd", function (req, res) {
+    res.send("Hello World from the asda endpoint.");
+})
+
+// Sending json. 
+app.post("/", function (req, res) {
+    res.json({
+        "hi": "k xa tmro";
+    });
+})
+
+// Sending html. 
+app.post("/", function (req, res) {
+    res.html("<b>Hi there.</b>");
+})
+
+app.post("/", function (req, res) {
+    res.send("Hello World from the post end point.");
+})
+
+app.listen(3000);
+
