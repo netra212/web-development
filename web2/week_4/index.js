ca0e3d5c35a97739e0d16fbe65c2d83029b4b04f
netra212 <netra200021kcbdr@gmail.com>
2025-07-28 15:43:11 +0100

    Adding week 4

---
+++ web2/week_4/index.js
@@ -0,0 +1,26 @@
+
+// import chalk from 'chalk';
+
+// // const chalk = require('chalk');
+
+// console.log(chalk.blue('Hello, world!'));
+// console.log(chalk.red.bold('This is an error message'));
+// console.log(chalk.green.underline('This is a success message.'));
+
+// const fs = require("fs");
+// const path = require("path");
+
+// const filepath = path.join(__dirname, "a.txt");
+
+// fs.readFile(filepath, 'utf8', (err, data) => {
+//     if (err){
+//         console.log("Issues reading file.");
+//     }
+//     else{
+//         console.log(data);
+//     }
+// })
+
+
+
+
