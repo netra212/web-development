a3b294f9e07cba2120d62a650e97a34ee2a28726
netra212 <netra200021kcbdr@gmail.com>
2025-07-28 19:13:50 +0100

    Adding node js

--- web2/week_4/index.js
+++ web2/week_4/index.js
@@ -21,6 +21,24 @@
 //     }
 // })
 
+// Create a Command Line Interface.: that lets the user specify a file path and 
+const fs = require("fs");
+const { Command } = require("commander");
 
+const program = new Command();
 
+program.name("counter").description("CLI to do file based tasks.").version("0.8.0");
 
+program.command("count").description("Count the number of lines in a file.").argument("<file>", "file to count").action((file) => {
+    fs.readFile("a.txt", "utf-8", (err, data) => {
+        if (err){
+            console.log(err);
+        }
+        else{
+            const lines = data.split("\n").length;
+            console.log(`There are ${lines} lines in ${file}`);
+        }
+    })
+})
+
+program.parse();
