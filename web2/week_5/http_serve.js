7d9f1051072f1040bc44c916a50fb42610bb36ae
netra212 <netra200021kcbdr@gmail.com>
2025-07-31 22:35:46 +0100

    creating basic 5 http server

---
+++ web2/week_5/http_serve.js
@@ -0,0 +1,42 @@
+
+const express = require("express");
+const app = express();
+
+app.get("/sum", function(req, res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    });
+
+});
+
+app.get("/multiply", function(req, res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a * b
+    })
+});
+
+app.get("/divide", function(req, res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a / b
+    })
+});
+
+app.get("/substract", function(req, res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a - b
+    })
+})
+
+app.listen(3001);
