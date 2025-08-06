00a19c2935d9da9b44dd70e841d66c4f1cb1da03
netra212 <netra200021kcbdr@gmail.com>
2025-08-06 14:28:36 +0100

    feat (auth)

---
+++ web2/week_7/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+app.post("/signup", function(req, res){
+
+})
+
+app.post("/signin", function (req, res) {
+
+})
+
+app.get("/todo", function (req, res) {
+
+})
+
+app.get("/todos", function(req, res){
+
+})
