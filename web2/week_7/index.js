7da3d6c3505885b77e887c6ae6844f1effc38b03
netra212 <netra200021kcbdr@gmail.com>
2025-08-07 12:39:35 +0100

    feat (auth)

--- web2/week_7/index.js
+++ web2/week_7/index.js
@@ -1,8 +1,25 @@
-const mongoose = require("mongoose");
 const express = require("express");
+const { UserModel, TodoModel } = require("./db");
+
+const app = express();
+
+app.use(express.json());
 
 app.post("/signup", function(req, res){
 
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    UserModel.insert({
+        email: email,
+        password: password,
+        name: name
+    })
+
+    res.json({
+        message: "You are logged in."
+    })
 })
 
 app.post("/signin", function (req, res) {
