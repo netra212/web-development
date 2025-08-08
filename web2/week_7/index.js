562e5f6dc5a72fad48b501c295c879208477f894
netra212 <netra200021kcbdr@gmail.com>
2025-08-08 09:30:10 +0100

    feat (auth)

--- web2/week_7/index.js
+++ web2/week_7/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "asdas123@gmail.com";
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://kcnetrabahadur98:fCytJWdySSlNA5mZ@cluster0.dmfas0c.mongodb.net/todo-netra-2");
 
 const app = express();
-
 app.use(express.json());
 
-app.post("/signup", function(req, res){
-
+app.post("/signup", async function (req, res) {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-    UserModel.insert({
+    // return Promise. 
+    await UserModel.create({
         email: email,
         password: password,
         name: name
@@ -20,16 +24,73 @@ app.post("/signup", function(req, res){
     res.json({
         message: "You are logged in."
     })
-})
-
-app.post("/signin", function (req, res) {
 
 })
 
-app.get("/todo", function (req, res) {
+app.post("/signin", async function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    const user = await UserModel.findOne({
+        email: email,
+        password: password
+    })
+
+    console.log(user);
+
+    if (user) {
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, JWT_SECRET);
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
 })
 
-app.get("/todos", function(req, res){
+app.get("/todo", auth, async function (req, res) {
+    const userId = req.userId;
+    const title = req.body.title;
 
+    await TodoModel.create({
+        title,
+        userId
+    })
+
+    res.json({
+        userId: userId
+    })
 })
+
+app.get("/todos", auth, async function (req, res) {
+    const userId = req.userId;
+
+    const users = await TodoModel.find({
+
+    })
+
+    res.json({
+        userId: userId
+    })
+})
+
+function auth(req, res, next) {
+    const token = req.headers.token;
+
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if (decodedData) {
+        req.userId = decodedData.id;
+        next();
+    } else {
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
+}
+
+app.listen(3002);
