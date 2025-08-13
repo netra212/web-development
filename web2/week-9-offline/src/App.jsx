e307a4da7ddd2b6e9bc6268575e8985250522654
netra212 <netra200021kcbdr@gmail.com>
2025-08-13 23:52:13 +0100

    React

---
+++ web2/week-9-offline/src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react"
+
+// App Component.
+function App() {
+  return (
+    <div>
+      <ToogleMessage></ToogleMessage>
+    </div>
+  )
+}
+
+const ToogleMessage = () => {
+  // const [isVisible, setIsVisible] = useState(false); // Defining a new state variable. 
+
+  let [isVisible, setIsVisible] = useState();
+
+  function toogle() {
+    setIsVisible(!isVisible);
+  }
+
+  return (
+    <div>
+      <button onClick={toogle}>
+        Toogle Message
+      </button>
+      {isVisible && <p>This message is conditionally rendered !</p>}
+    </div>
+  );
+};
+
+// function PostComponent({ name, subtitle, time, image, description }) {
+//   return <div style={style}>
+//     <div style={{ display: "flex" }}>
+//       <img src={image} style={{
+//         width: 30,
+//         height: 30,
+//         borderRadius: 20
+//       }} />
+//       <div style={{ fontSize: 10, marginLeft: 10 }}>
+//         <b>{name}</b>
+//         <div>{subtitle}</div>
+//         {(time !== undefined) ?
+//           <div style={{ display: "flex" }}>
+//             <div>{time}</div>
+//             <img src="clock_image.png " style={{ width: 20, height: 12 }} />
+//           </div>: null 
+//         }
+//       </div>
+//     </div>
+//     <div style={{ fontSize: 12 }}>
+//       {description}
+//     </div>
+//   </div>
+// }
+
+// const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, padding: 20 }
+
+/* <PostComponent
+            name={"netra"}
+            subtitle={"follower count"}
+            time={"3m ago"}
+            image={"100xDevs.png"}
+            description={"How to get hired in 2024 ? I lost my job in 2023, this is the roadmap I followed to get a job."}
+          ></PostComponent>
+          <br />
+        </div>
+        <div>
+          <PostComponent
+            name={"yagya"}
+            subtitle={"Promoted"}
+            time={"4m ago"}
+            image={"100xDevs.png"}
+            description={"What to know to to win big ? Check but how these folks won $6000 in bounties. "}
+          ></PostComponent>
+          <br /> */
+
+export default App
+
+
