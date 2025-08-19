0c49589c7a2a6962ddc624b4798962924c37d1a2
netra212 <netra200021kcbdr@gmail.com>
2025-08-19 21:02:55 +0100

    Building Posts

--- web2/week-9-offline/src/App.jsx
+++ web2/week-9-offline/src/App.jsx
@@ -1,82 +1,37 @@
-import { useState } from "react"
 
-// App Component.
+import { useState } from "react";
+import { PostComponent } from "./Post";
+
 function App() {
-  return (
-    <div style={{ background: "#dfe6e9", height: "100vh"}}>
-      <ToogleMessage></ToogleMessage>
-      <ToogleMessage></ToogleMessage>
-      <ToogleMessage></ToogleMessage>
-    </div>
-  )
-}
 
-const ToogleMessage = () => {
-  // const [isVisible, setIsVisible] = useState(false); // Defining a new state variable. 
+  const [posts, setPosts] = useState([]);
 
-  // let [isVisible, setIsVisible] = useState();
-  let [notificationCount, setNotificationCount] = useState(0);
+  const postComponent = posts.map(post => <PostComponent
+    name={posts.name}
+    subtitle={posts.subtitle}
+    time={posts.title}
+    image={posts.image}
+    description={posts.description}
+  ></PostComponent>)
 
-  function increment() {
-    setNotificationCount(notificationCount + 1);
+  function addPost() {
+    posts.push({
+      name: "netra",
+      subtitle: "1000 followers",
+      time: "2m ago",
+      image: "/Users/user/Desktop/web-development/web2/week-9-offline/src/LinkedIn.png",
+      description: "what to know how to win big ? Check out how these folks won $6000 in bounties."
+    })
   }
 
   return (
-    <div>
-      <button onClick={increment}>
-        Increased Count
-      </button>
-      {notificationCount}
-    </div>
-  );
-};
-
-// function PostComponent({ name, subtitle, time, image, description }) {
-//   return <div style={style}>
-//     <div style={{ display: "flex" }}>
-//       <img src={image} style={{
-//         width: 30,
-//         height: 30,
-//         borderRadius: 20
-//       }} />
-//       <div style={{ fontSize: 10, marginLeft: 10 }}>
-//         <b>{name}</b>
-//         <div>{subtitle}</div>
-//         {(time !== undefined) ?
-//           <div style={{ display: "flex" }}>
-//             <div>{time}</div>
-//             <img src="clock_image.png " style={{ width: 20, height: 12 }} />
-//           </div>: null 
-//         }
-//       </div>
-//     </div>
-//     <div style={{ fontSize: 12 }}>
-//       {description}
-//     </div>
-//   </div>
-// }
-
-// const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, padding: 20 }
-
-/* <PostComponent
-            name={"netra"}
-            subtitle={"follower count"}
-            time={"3m ago"}
-            image={"100xDevs.png"}
-            description={"How to get hired in 2024 ? I lost my job in 2023, this is the roadmap I followed to get a job."}
-          ></PostComponent>
-          <br />
-        </div>
+    <div style={{ background: "#def6e9", height: "100vh" }}>
+      <button onClick={addPost}>Add Post</button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <div>
-          <PostComponent
-            name={"yagya"}
-            subtitle={"Promoted"}
-            time={"4m ago"}
-            image={"100xDevs.png"}
-            description={"What to know to to win big ? Check but how these folks won $6000 in bounties. "}
-          ></PostComponent>
-          <br /> */
-
-export default App
-
-
+          {postComponent}
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ web2/week-9-offline/src/Post.jsx
@@ -0,0 +1,11 @@
+
+const style = {
+    width: 200,
+    backgroundColor: "white",
+    borderRadius: 10, 
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 20
+}
+
+
